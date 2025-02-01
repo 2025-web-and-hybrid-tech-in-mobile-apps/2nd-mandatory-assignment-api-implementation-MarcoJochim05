5349b9fbb563a8cba56dde5a57366d84d7f4254a
MarcoJochim05 <marco.jochim@student.hnu.de>
2025-02-01 23:07:40 +0100

    Working 2

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post("/login", (req, res) => {
 
   const token = username + Date.now();
   authorizedTokens.push(token);
-  res.status(200).json({ "Login successful, JWT token provided; jsonWebToken": token });
+  res.status(200).json({ "Login successful, JWT token provided": token });
 });
 
 app.post("/high-scores", (req, res) => {
